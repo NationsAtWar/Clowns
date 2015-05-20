240a91185c12193daef7134904192cff6ee298ca
Matt Luesing <Kainte@gmail.com>
2015-05-19 22:06:06 -0700

    Setup project workspace

---
+++ src/main/java/org/nationsatwar/clowns/Clowns.java
@@ -0,0 +1,48 @@
+package org.nationsatwar.clowns;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import org.nationsatwar.clowns.proxy.CommonProxy;
+
+@Mod(modid = Clowns.MODID, 
+	name = Clowns.MODNAME, 
+	version = Clowns.MODVER)
+public class Clowns {
+
+    @Instance(Clowns.MODID)
+    public static Clowns instance;
+
+	@SidedProxy(clientSide = Clowns.CLIENT_PROXY_CLASS, serverSide = Clowns.SERVER_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	public static final String MODID = "clowns";
+	public static final String MODNAME = "Clowns";
+	public static final String MODVER = "0.0.1";
+	
+	public static final String CLIENT_PROXY_CLASS = "org.nationsatwar.clowns.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "org.nationsatwar.clowns.proxy.CommonProxy";
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+		
+	}
+}
---
+++ src/main/java/org/nationsatwar/clowns/proxy/CommonProxy.java
@@ -0,0 +1,8 @@
+package org.nationsatwar.clowns.proxy;
+
+public class CommonProxy {
+	
+	public void registerKeybindings() {}
+	public void registerClientEvents() {}
+	public void registerGUI() {}
+}
